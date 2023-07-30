cd271eeade19d0976719720bb8d8f098689b9ad9
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2023-07-31 04:22:24 +0545

    route fix test

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
     { href: '/blog', title: 'Publications' },
     { href: '/tags', title: 'Tags' },
     { href: '/about', title: 'About' },
-    { href: '/blog', title: 'Publications' },
+    { href: '/profile', title: 'Profile' },
     { href: '/tags', title: 'Tags' },
 
     
