338a31a878815284a451086f9ea033f5e244fbe9
abhinavpatel0 <121202898+abhinavpatel0@users.noreply.github.com>
2023-08-04 14:16:45 +0530

    Delete components directory

--- components/icons/Cross.jsx
+++
@@ -1,8 +0,0 @@
-export default function Cross() {
-    return(
-        <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-            <path d="M 10 10 L 90 90"></path>
-            <path d="M 90 10 L 10 90"></path>
-        </svg>
-    );
-}
